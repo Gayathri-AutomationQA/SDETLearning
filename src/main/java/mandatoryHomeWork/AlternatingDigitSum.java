6b1dff300b2bc486be3cd6abf3d1dbd4a38066cc
Gayathri-AutomationQA <103032623+Gayathri-AutomationQA@users.noreply.github.com>
2023-08-02 22:48:38 +0530

    Update AlternatingDigitSum.java
    Commented Approach 2 as it is pending

--- src/main/java/mandatoryHomeWork/AlternatingDigitSum.java
+++ src/main/java/mandatoryHomeWork/AlternatingDigitSum.java
@@ -4,29 +4,29 @@ public class AlternatingDigitSum { public int alternateDigitSum(int n) {
 	
 	//Approach one
 
-//	String s = String.valueOf(n);
-//	int sum = 0;
-//	for (int i = 0; i < s.length(); i++) {
-//		if(i%2!=0) {
-//			sum = sum - (s.charAt(i)-'0');
-//		}else {
-//			sum = sum - (s.charAt(i)-'0');
-//		}
-//
-//	}
-	
-	//Approach 2
+	String s = String.valueOf(n);
 	int sum = 0;
-	int j =0;
-	while (n>0) {
-			n = n/10;
-			if(j%2!=0) {
-				sum = sum - n;
-			}else {
-				sum = sum + n;
-			}
-			j++;
+	for (int i = 0; i < s.length(); i++) {
+		if(i%2!=0) {
+			sum = sum - (s.charAt(i)-'0');
+		}else {
+			sum = sum - (s.charAt(i)-'0');
+		}
+
 	}
+	
+	//Approach 2 -- Pending
+	// int sum = 0;
+	// int j =0;
+	// while (n>0) {
+	// 		n = n/10;
+	// 		if(j%2!=0) {
+	// 			sum = sum - n;
+	// 		}else {
+	// 			sum = sum + n;
+	// 		}
+	// 		j++;
+	// }
 	
 	return sum;
 }
