0696df7d63b4a1c575ff1a521b12ee2a78ae2be6
bgayathriofficial@gmail.com <bgayathriofficial@gmail.com>
2023-08-18 20:29:07 +0530

    Aug-18_SpyNumber

---
+++ src/main/java/mandatoryHomeWork/SpyNumber.java
@@ -0,0 +1,26 @@
+package mandatoryHomeWork;
+
+import org.testng.annotations.Test;
+
+public class SpyNumber {
+	
+	public boolean spyNumber(int n) {
+		char[] arrNew = String.valueOf(n).toCharArray();
+		int sum = 0;
+		int product =1;
+		for (int i = 0; i < arrNew.length; i++) {
+			sum += arrNew[i]-'0';
+			product*=arrNew[i]-'0';
+		}
+		if(sum==product) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	@Test
+	public void Test1() {	
+		boolean actual = spyNumber(1000);
+		System.out.println(actual);
+	}
+}
